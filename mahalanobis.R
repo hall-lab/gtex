ada6b462d7459c95e5210b8af7b19e82fd4f9a15
Colby Chiang <cc2qe@virginia.edu>
2015-08-13 15:23:54 -0500

    ignore those with <2 tissues of the 13

--- mahalanobis.R
+++ mahalanobis.R
@@ -13,47 +13,47 @@ set.seed(10)
 suppressMessages(library('gplots'))
 
 imp.col <- function (a) {
-	imputed <- a
-	for (j in 1:ncol(a)) {
-		missing <- is.na(a[,j])
-		n.missing <- sum(missing)
-		obs <- a[,j][!missing]
-		imputed[,j][missing] <- sample(obs, n.missing, replace=TRUE)
-	}
-	return(imputed)
+    imputed <- a
+    for (j in 1:ncol(a)) {
+        missing <- is.na(a[,j])
+        n.missing <- sum(missing)
+        obs <- a[,j][!missing]
+        imputed[,j][missing] <- sample(obs, n.missing, replace=TRUE)
+    }
+    return(imputed)
 }
 
 imp.colmean <- function (a) {
-	imputed <- a
-	for (j in 1:ncol(a)) {
-		missing <- is.na(a[,j])
-		n.missing <- sum(missing)
-		obs <- a[,j][!missing]
-		imputed[,j][missing] <- mean(obs)
-	}
-	return(imputed)
+    imputed <- a
+    for (j in 1:ncol(a)) {
+        missing <- is.na(a[,j])
+        n.missing <- sum(missing)
+        obs <- a[,j][!missing]
+        imputed[,j][missing] <- mean(obs)
+    }
+    return(imputed)
 }
 
 imp.row <- function (a) {
-	imputed <- a
-	for (i in 1:nrow(a)) {
-		missing <- is.na(a[i,])
-		n.missing <- sum(missing)
-		obs <- a[i,][!missing]
-		imputed[i,][missing] <- sample(obs, n.missing, replace=TRUE)
-	}
-	return(imputed)
+    imputed <- a
+    for (i in 1:nrow(a)) {
+        missing <- is.na(a[i,])
+        n.missing <- sum(missing)
+        obs <- a[i,][!missing]
+        imputed[i,][missing] <- sample(obs, n.missing, replace=TRUE)
+    }
+    return(imputed)
 }
 
 imp.rowmean <- function (a) {
-	imputed <- a
-	for (i in 1:nrow(a)) {
-		missing <- is.na(a[i,])
-		n.missing <- sum(missing)
-		obs <- a[i,][!missing]
-		imputed[i,][missing] <- mean(obs)
-	}
-	return(imputed)
+    imputed <- a
+    for (i in 1:nrow(a)) {
+        missing <- is.na(a[i,])
+        n.missing <- sum(missing)
+        obs <- a[i,][!missing]
+        imputed[i,][missing] <- mean(obs)
+    }
+    return(imputed)
 }
 
 
@@ -73,57 +73,65 @@ header <- unlist(strsplit(readLines(f,n=1), '\t'))
 
 # for (i in 1:nrow(full)) {
 while(length(line <- readLines(f,n=1)) > 0) {
-	v <- matrix(unlist(strsplit(line, '\t')), nrow=1)
-	gene <- v[1]
+    v <- matrix(unlist(strsplit(line, '\t')), nrow=1)
+    gene <- v[1]
 
-	if (!is.null(prev.gene) && gene != prev.gene) {
-		rownames(x) <- x[,2]
-		colnames(x) <- header
-		
-		x <- x[,-(1:2)]
-		x <- as.matrix(x)
-		suppressWarnings(class(x) <- 'numeric')
-		x <- t(x)
-		# x <- x[which(apply(x, 1, function(z) sum(!is.na(z))) >= min.nsamp),]
-		x <- x[, which(colnames(x) %in% tissue.list)]
-                print(x)
-                print(which(colnames(x) %in% tissue.list))
-                print(ncol(x))
-                print(is.null(x))
-                if (!is.null(x) && ncol(x) > 0) {
-                    x <- imp.col(x)
+    if (!is.null(prev.gene) && gene != prev.gene) {
+        ## print(prev.gene)
+        rownames(x) <- x[,2]
+        colnames(x) <- header
+        ## print(x)
 
-                    ## png(paste0('../plots/', prev.gene, '.heatmap.png'), height=8, width=8, units='in', res=150)
-                    ## heatmap.2(x, trace='none', na.color='gray')
-                    ## dev.off()
+        x <- x[,-(1:2)]
+        x <- as.matrix(x)
 
-                    deg.fr <- ncol(x)
-                    mean <- colMeans(x)
-                    Sx <- cov(x)
-                    D2 <- mahalanobis(x, mean, Sx, tol=1e-20)
-                    ## pdf(paste0('../plots/', prev.gene, '.mahalanobis_d2.pdf'), height=8, width=8)
-                    ## plot(density(D2, bw = 0.5), main=paste0('Mahalanobis distance, ', prev.gene))
-                    ## rug(D2)
-                    ## dev.off()
+        suppressWarnings(class(x) <- 'numeric')
+        ## print('num')
+        ## print(x)
 
-                    ## pdf(paste0('../plots/', prev.gene, '.qq.pdf'), height=8, width=8)		
-                    ## qqplot(qchisq(ppoints(100), df = deg.fr), D2,
-                    ##        main = expression("Qs-Q plot of Mahalanobis" * ~D^2 *
-                    ##                          " vs. quantiles of" * ~ chi^2))
-                    ## abline(0, 1, col = 'gray')
-                    ## dev.off()
+        x <- t(x)
+        ## print('transpo')
+        ## print(x)
+        if (sum(colnames(x) %in% tissue.list) > 1) {
+            # x <- x[which(apply(x, 1, function(z) sum(!is.na(z))) >= min.nsamp),]
+            x <- x[, which(colnames(x) %in% tissue.list)]
+            ## print(x)
+            ## print(which(colnames(x) %in% tissue.list))
+            ## print(ncol(x))
+            ## print(is.null(x))
+            x <- imp.col(x)
 
-                    sig <- D2[D2>qchisq(0.999, ncol(x))]
-                    if (length(sig) > 0) {
-                            for (j in 1:length(sig)) {
-                                    cat(names(sig)[j], prev.gene, log(pchisq(sig[j], df=deg.fr, lower.tail=F), 10), '\n', sep='\t')
-                            }
-                    }
-                  }
-		x <- NULL
-	}
-	prev.gene <- gene
-	x <- rbind(x, v)
+            ## png(paste0('../plots/', prev.gene, '.heatmap.png'), height=8, width=8, units='in', res=150)
+            ## heatmap.2(x, trace='none', na.color='gray')
+            ## dev.off()
+
+            deg.fr <- ncol(x)
+            mean <- colMeans(x)
+            Sx <- cov(x)
+            D2 <- mahalanobis(x, mean, Sx, tol=1e-20)
+            ## pdf(paste0('../plots/', prev.gene, '.mahalanobis_d2.pdf'), height=8, width=8)
+            ## plot(density(D2, bw = 0.5), main=paste0('Mahalanobis distance, ', prev.gene))
+            ## rug(D2)
+            ## dev.off()
+
+            ## pdf(paste0('../plots/', prev.gene, '.qq.pdf'), height=8, width=8)        
+            ## qqplot(qchisq(ppoints(100), df = deg.fr), D2,
+            ##        main = expression("Qs-Q plot of Mahalanobis" * ~D^2 *
+            ##                          " vs. quantiles of" * ~ chi^2))
+            ## abline(0, 1, col = 'gray')
+            ## dev.off()
+
+            sig <- D2[D2>qchisq(0.999, ncol(x))]
+            if (length(sig) > 0) {
+                for (j in 1:length(sig)) {
+                        cat(names(sig)[j], prev.gene, log(pchisq(sig[j], df=deg.fr, lower.tail=F), 10), '\n', sep='\t')
+                }
+            }
+        }
+        x <- NULL
+    }
+    prev.gene <- gene
+    x <- rbind(x, v)
 }
 
 
