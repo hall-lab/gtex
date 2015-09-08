43d2fe55ac8b5a7393b458173ef5b9e87b6ad51c
Colby Chiang <cc2qe@virginia.edu>
2015-09-08 10:38:28 -0500

    use version controlled svscore executable

--- runsvscore/generatesvscorecalls.pl
+++ runsvscore/generatesvscorecalls.pl
@@ -3,11 +3,11 @@
 opendir(CWD, $ARGV[0]) || die "Could not open cwd: $!";
 @files = grep {/^split\d\d\.vcf$/} readdir CWD;
 
-print "/gscmnt/gc2719/halllab/users/lganel/SVScore/svscore.pl -s -g /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.genes.bed -e /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.exons.bed -n 4 -c /gscmnt/gc2719/halllab/users/lganel/SVScore/whole_genome_SNVs.tsv.gz -o max split00.vcf\n";
+print "/gscmnt/gc2719/halllab/users/cchiang/src/SVScore/svscore.pl -s -g /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.genes.bed -e /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.exons.bed -n 4 -c /gscmnt/gc2719/halllab/users/lganel/SVScore/whole_genome_SNVs.tsv.gz -o max split00.vcf\n";
 
 foreach (sort @files) {
   ($prefix) = /(.*)\.vcf/;
-  print "bomb -J $prefix -m 4 -e $prefix.log -o $prefix.log -q hall-lab \"/gscmnt/gc2719/halllab/users/lganel/SVScore/svscore.pl -g /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.genes.bed -e /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.exons.bed -n 4 -c /gscmnt/gc2719/halllab/users/lganel/SVScore/whole_genome_SNVs.tsv.gz -o max $_ > $prefix.out.vcf\"\n";
+  print "bomb -J $prefix -m 4 -e $prefix.log -o $prefix.log -q hall-lab \"/gscmnt/gc2719/halllab/users/cchiang/src/SVScore/svscore.pl -g /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.genes.bed -e /gscmnt/gc2719/halllab/users/lganel/SVScore/gencode.v19.exons.bed -n 4 -c /gscmnt/gc2719/halllab/users/lganel/SVScore/whole_genome_SNVs.tsv.gz -o max $_ > $prefix.out.vcf\"\n";
 }
 
 
