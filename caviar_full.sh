71263212cea8490bb0299b26f938711f985c28ad
Colby Chiang <cc2qe@virginia.edu>
2016-10-25 19:06:29 -0500

    add ds

--- caviar_full.sh
+++ caviar_full.sh
@@ -7,6 +7,12 @@ then
 fi
 
 LIST=$1
+NUMCAUSAL=$2
+
+if [[ -z "$NUMCAUSAL" ]]
+then
+    NUMCAUSAL=1
+fi
 
 # job index
 EGENE=`zless $LIST | sed -n ${LSB_JOBINDEX},${LSB_JOBINDEX}p | cut -f 1`
@@ -46,11 +52,12 @@ cat tissues/$TISSUE/$EGENE/$EGENE.nom.eqtl.txt \
 REGION=`zcat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/data/GTEx_Analysis_2015-01-12/eqtl_data/eQTLInputFiles_genePositions/GTEx_Analysis_2015-01-12_eQTLInputFiles_genePositions.txt.gz | awk -v EGENE=$EGENE -v SLOP=10000000 '{ if (\$1==EGENE) { POS_START=\$3-SLOP; POS_END=\$4+SLOP; if (POS_START<0) POS_START=0; print \$2":"POS_START"-"POS_END } }'`;
 
 tabix -h ../../gtex.joint_sv_gatk.vcf.gz $REGION \
+    | awk '{ if ($0~"^#" || ($3!~"^LUMPY" && $3!~"^GS")) { print; next } $9=$9":DS"; split($9,fmt,":"); if ($3~"^LUMPY") { field="AB"; scalar=2 } if ($3~"^GS_") { field="CN"; scalar=1 } for (i=1;i<=length(fmt);++i) { if (fmt[i]==field) fmt_idx=i } for (i=10;i<=NF;++i) { split($i,gt,":"); $i=$i":"gt[fmt_idx]*scalar } print }' OFS="\t" \
     | /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/src/ld_continuous.py -v tissues/$TISSUE/$EGENE/vars.txt -f DS -a r \
     > tissues/$TISSUE/$EGENE/vars.cts.ld
 
 # ---------------------------------------
 # run caviar
-CAVIAR -o tissues/$TISSUE/$EGENE/caviar.t-stat.cts -l tissues/$TISSUE/$EGENE/vars.cts.ld -z tissues/$TISSUE/$EGENE/t-stat.txt -c 1
+CAVIAR -o tissues/$TISSUE/$EGENE/caviar.t-stat.cts -l tissues/$TISSUE/$EGENE/vars.cts.ld -z tissues/$TISSUE/$EGENE/t-stat.txt -c $NUMCAUSAL
 
 
